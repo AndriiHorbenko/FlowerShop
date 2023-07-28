5daa8cc3319d6a65999f0c3a1947fed892143347
AndriiHorbenko <118114091+AndriiHorbenko@users.noreply.github.com>
2023-07-28 14:23:05 +0300

    Us6 adding checkstyle, travis, build, sonar properties (#3)

--- src/main/java/com/shop/flowershop/FlowerShopApplication.java
+++ src/main/java/com/shop/flowershop/FlowerShopApplication.java
@@ -1,13 +1,11 @@
-package com.shop.FlowerShop;
+package com.shop.flowershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowerShopApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerShopApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FlowerShopApplication.class, args);
+  }
 }
--- src/test/java/com/shop/flowershop/FlowerShopApplicationTests.java
+++ src/test/java/com/shop/flowershop/FlowerShopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shop.FlowerShop;
+package com.shop.flowershop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
