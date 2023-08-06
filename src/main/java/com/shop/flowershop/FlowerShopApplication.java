99da4cb10f1070b3723b45c0e76e9b381632af88
Oleksandr Khmarenko <khmarenko.a@gmail.com>
2023-08-06 15:34:33 +0300

    [US9-10] Add DB connection, Add environmental variables

--- src/main/java/com/shop/flowershop/FlowerShopApplication.java
+++ src/main/java/com/shop/flowershop/FlowerShopApplication.java
@@ -1,11 +1,14 @@
 package com.shop.flowershop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowerShopApplication {
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().load();
+    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
     SpringApplication.run(FlowerShopApplication.class, args);
   }
 }
